5ad7eb34bd1c2889ded63af609b71592811b76b5
devmohy <yayahmohammed@gmail.com>
2019-10-22 23:13:51 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/chatscrum'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/chatscrum/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/chatscrum/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
